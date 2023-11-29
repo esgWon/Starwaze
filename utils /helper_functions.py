56afc46d82117410cd68b2e78a3da9aa28e68a42
esgWon <152344401+esgWon@users.noreply.github.com>
2023-11-28 18:43:29 -0700

    Create helper_functions.py

---
+++ utils /helper_functions.py
@@ -0,0 +1,15 @@
+# src/utils/helper_functions.py
+
+def load_dataset():
+    # Implement logic to load datasets
+    pass
+
+def save_model(model, filepath):
+    # Implement logic to save trained models
+    pass
+
+def evaluate_model(model, test_data):
+    # Implement logic to evaluate models
+    pass
+
+# Implement other utility functions as needed
